f6e894ed7ba390d770c35b1181dfc5611616a321
Hung <skellybg245@gmail.com>
2025-05-04 22:10:37 +1000

    feat: add nav bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://skellybg.github.io',
-    base: 'astro-blog',
+  site: 'https://skellybg.github.io',
+  base: 'astro-blog',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
